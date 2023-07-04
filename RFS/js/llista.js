59b88ea7af973a97eaf70582c16843c23f7ae7dd
Tomas Bernaus Sole <a211426tb@correu.escoladeltreball.org>
2023-07-04 14:13:35 +0200

    llistat

---
+++ RFS/js/llista.js
@@ -0,0 +1,106 @@
+let ingredients = [
+    "pa",
+    "formatge",
+    "base",
+    "verdureta",
+    "wildMagic",
+    "salsa"
+]
+
+let pa = [
+    "Pagès",
+    "Baguette",
+    "Motlle",
+    "Bagel",
+    "Llavors",
+    "Hamburguesa",
+    "Pita",
+    "Frankfurt"
+];
+
+
+let formatge = [
+    "Fresc",
+    "Tendre",
+    "Rulo de cabra",
+    "Semicurat",
+    "Grana Padano",
+    "Gouda",
+    "Havarti",
+    "Edam",
+    "Cheddar",
+    "Emmental",
+    "Curat",
+    "Cabra en llesques"
+]
+
+
+let base = [
+    "Pernil salat",
+    "Pernil dolç",
+    "Llom",
+    "Mortadela",
+    "Tonyina",
+    "Pollastre",
+    "Frankfurt",
+    "Bacó",
+    "Hamburguesa",
+    "Carn arrebossada",
+    "Hamburguesa vegetal",
+    "Rodó de pollastre",
+    "Mandonguilles",
+    "Tires vegetals",
+    "Botifarra",
+    "Tofu",
+    "Carn picada",
+    "Botifarra blanca",
+    "Botifarra d'ou",
+    "Fuet"
+]
+
+let verdureta = [
+    "Enciam",
+    "Ceba",
+    "Tomàquet",
+    "Escalivada",
+    "Canonges",
+    "Rúcula"
+]
+
+let wildMagic = [
+    "Romaní",
+    "All en pols",
+    "Pebre",
+    "Farigola",
+    "Papinillu",
+    "Olives",
+    "Orenga",
+    "Extra salsa",
+    "Extra formatge",
+    "Ceba cruixent",
+    "Pinyons",
+    "Nous"
+]
+
+let salsa = [
+    "Pesto",
+    "Bolets",
+    "Formatges",
+    "Tomàquet",
+    "Guacamole",
+    "Cesar",
+    "Romesco",
+    "Còctel",
+    "Vinagre",
+    "Mango i curry",
+    "Olivada",
+    "Barbacoa",
+    "Allioli",
+    "Mostassa",
+    "Brava",
+    "Maionesa",
+    "Teriyaki",
+    "Quètxup",
+    "Hummus",
+    "Espinaler"
+]
--- RFS/js/main.js
+++ RFS/js/main.js
@@ -1,130 +1,18 @@
-let ingredients = [
-    "pa",
-    "formatge",
-    "base",
-    "verdureta",
-    "wildMagic",
-    "salsa",
-]
-
-let pa = [
-    "Pagès",
-    "Baguette",
-    "Motlle",
-    "Bagel",
-    "Llavors",
-    "Hamburguesa",
-    "Pita",
-    "Frankfurt",
-];
-
-
-let formatge = [
-    "Fresc",
-    "Tendre",
-    "Rulo de cabra",
-    "Semicurat",
-    "Grana Padano",
-    "Gouda",
-    "Havarti",
-    "Edam",
-    "Cheddar",
-    "Emmental",
-    "Curat",
-    "Cabra en llesques",
-]
-
-
-let base = [
-    "Pernil salat",
-    "Pernil dolç",
-    "Llom",
-    "Mortadela",
-    "Tonyina",
-    "Pollastre",
-    "Frankfurt",
-    "Bacó",
-    "Hamburguesa",
-    "Carn arrebossada",
-    "Hamburguesa vegetal",
-    "Rodó de pollastre",
-    "Mandonguilles",
-    "Tires vegetals",
-    "Botifarra",
-    "Tofu",
-    "Carn picada",
-    "Botifarra blanca",
-    "Botifarra d'ou",
-    "Fuet",
-]
-
-let verdureta = [
-    "Enciam",
-    "Ceba",
-    "Tomàquet",
-    "Escalivada",
-    "Canonges",
-    "Rúcula",
-]
-
-let wildMagic = [
-    "Romaní",
-    "All en pols",
-    "Pebre",
-    "Farigola",
-    "Papinillu",
-    "Olives",
-    "Orenga",
-    "Extra salsa",
-    "Extra formatge",
-    "Ceba cruixent",
-    "Pinyons",
-    "Nous",
-]
-
-let salsa = [
-    "Pesto",
-    "Bolets",
-    "Formatges",
-    "Tomàquet",
-    "Guacamole",
-    "Cesar",
-    "Romesco",
-    "Còctel",
-    "Vinagre",
-    "Mango i curry",
-    "Olivada",
-    "Barbacoa",
-    "Allioli",
-    "Mostassa",
-    "Brava",
-    "Maionesa",
-    "Teriyaki",
-    "Quètxup",
-    "Hummus",
-    "Espinaler",
-]
-
 function llistar(ingredient) {
-    ingredient.forEach(element => {
-        document.write(element + ", ")
-    });
+    // ingredient.forEach(element => {
+    //     document.write(element + ", ")
+    // });
+    document.write(ingredient)
+}
+
+function rellistar(ingredient){
+    document.getElementById('Llista').innerHTML= ingredient;
 }
 
 function escollir(ingredient) {
     const num = Math.floor(Math.random() * ingredient.length);
     let ingredientRandom = ingredient[num]
     document.write(ingredientRandom);
-}
-
-function lastWord(words) {
-    let n = words.replace(/[\[\]?.,\/#!$%\^&\*;:{}=\\|_~()]/g, "").split(" ");
-    return n[n.length - 1];
-}
-
-let comptador = 3;
-function counter(){
-    document.getElementById('comptador').innerHTML = comptador
 }
 
 function reroll(ingredient, divID) {
@@ -138,13 +26,4 @@ function reroll(ingredient, divID) {
         document.getElementById(divID).innerHTML = ingredientRandom
         document.getElementById('comptador').innerHTML = comptador
     }
-}
-
-function extra(){
-    comptador = comptador+1
-    document.getElementById('comptador').innerHTML = comptador
-}
-function infinit(){
-    comptador = comptador+99
-    document.getElementById('comptador').innerHTML = comptador
 }
