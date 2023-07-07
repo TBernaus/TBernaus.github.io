a99765bc7dbd86ecebfa3ba756215a2c19719cef
a211426tb <a211426tb@edt.org>
2023-07-07 14:27:21 +0200

    amagat

--- RFS/js/roll.js
+++ RFS/js/roll.js
@@ -1,20 +1,30 @@
+let amagat = true;
+
 function descobreix(ingredient, ingDIV, botoID) {
     text = `<button onclick="reroll(${ingDIV}, '${ingDIV}')">Reroll</button>`
     const num = Math.floor(Math.random() * ingredient.length);
     let ingredientRandom = ingredient[num]
     document.getElementById(ingDIV).innerHTML = ingredientRandom
     document.getElementById(botoID).innerHTML = text
+    amagat = false
 }
 
 function desbloca() {
+    if (amagat == true){
+
         let llistat = [pa, formatge, base, verdureta, wildMagic, salsa, pizza]
         let i=0
         ingredients.forEach(el => {
             descobreix(llistat[i], el, 'boto'+el)
             i++
         });
-        let boto = document.getElementById("desbloca");
-        boto.remove();
+        // let boto = document.getElementById("desbloca");
+        // boto.remove();
+        amagat = false
+    }
+    else(
+        amagat = false
+    )
 }
 
 function reroll(ingredient, divID) {
