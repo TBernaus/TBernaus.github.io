fd63367752fa1123f5566e348cd90ca5fb18a40f
TBernaus <tomasbernaussole@gmail.com>
2024-03-12 10:17:29 +0100

    diego

--- lorcanadex/scripts/modal.js
+++ lorcanadex/scripts/modal.js
@@ -1,28 +1,31 @@
 const fileListElement = document.getElementById('file-list');
+const modal = document.getElementById('modal');
+const modalImg = document.getElementById('modal-img');
 
-function showModal(imageSrc) {
-    const modal = document.getElementById('modal');
-    const modalImg = document.getElementById('modal-img');
+function showModal(imageSrc) 
+{
     modalImg.src = imageSrc;
     modal.style.display = 'block';
-    modal.addEventListener('click', closeModal);
 }
 
-function closeModal() {
-    const modal = document.getElementById('modal');
+function closeModal() 
+{
     modal.style.display = 'none';
-    modal.removeEventListener('click', closeModal);
 }
 
-fileListElement.addEventListener('click', function(event) {
-    if (event.target.tagName === 'IMG') {
+modal.addEventListener('click', closeModal);
+
+fileListElement.addEventListener('click', function(event) 
+{
+    if (event.target.tagName === 'IMG') 
+    {
         const imageSrc = event.target.src;
         showModal(imageSrc);
     }
 });
-window.addEventListener('click', function(event) {
-    const modal = document.getElementById('modal');
-    if (event.target === modal) {
-        closeModal();
-    }
+
+window.addEventListener('click', function(event) 
+{
+    if (event.target === modal) closeModal();
 });
+
