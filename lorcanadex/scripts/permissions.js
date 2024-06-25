bf6deb64046a844bbb0c42ad9bddda63bdb2b50c
TBernaus <tomasbernaussole@gmail.com>
2024-06-25 22:51:53 +0200

    prova permissions

--- lorcanadex/scripts/permissions.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('meta[http-equiv="Permissions-Policy"]').forEach((meta) => {
-        meta.setAttribute('content', 'interest-cohort=(), attribution-reporting=(), run-ad-auction=(), private-state-token-redemption=(), private-state-token-issuance=(), join-ad-interest-group=(), compute-pressure=(), browsing-topics=()');
-    });
-});
